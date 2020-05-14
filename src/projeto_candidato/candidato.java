5412c29f1d23a29a8c28199f032b24480eba0930
Guilherme Staciarini <staciarinineto@gmail.com>
2020-05-14 00:04:37 -0300

    Leitura dos outros dados

--- src/projeto_candidato/candidato.java
+++ src/projeto_candidato/candidato.java
@@ -5,26 +5,37 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class candidato {
-	
+
 	public static void main(String[] args) {
-		    
-			Locale.setDefault(Locale.US); // idioma US
-			Scanner sc = new Scanner(System.in); // leitura
-			
-			int ESCOLARIDADE; // variavel
-			
-			// mostra as opções
-	        System.out.println("Qual sua escolaridade?");
-	        System.out.println("1) Ensino Fundamental");
-	        System.out.println("2) Ensino Médio");
-	        System.out.println("3) Ensino Superior");
-	        System.out.println("4) Pós-graduação");
-	        System.out.print("Digite uma opção: ");
-	        
-	        // ler a resposta
-	        ESCOLARIDADE = sc.nextInt();
-	        
-	        sc.close();
-	        
-	}        
+
+		Locale.setDefault(Locale.US); // idioma US
+		Scanner sc = new Scanner(System.in); // leitura
+
+		int ESCOLARIDADE; // variavel
+		double EXPPRO;
+		char VIAJEM, HABILITACAO;
+
+		// mostra as opções
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.print("Digite uma opção: ");
+
+		// ler a escolaridade
+		ESCOLARIDADE = sc.nextInt();
+
+		System.out.print("Quantos anos de experiência profissional? ");
+		EXPPRO = sc.nextDouble();
+
+		System.out.print("Você tem disponibilidade para viajar? (S/N) ");
+		VIAJEM = sc.next().charAt(0);
+
+		System.out.print("Você tem habilitação de motorista? (S/N) ");
+		HABILITACAO = sc.next().charAt(0);
+
+		sc.close();
+
+	}
 }
