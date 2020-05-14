a4ecbb9a8e8e88e0f1876b956a41ce9d85199463
Guilherme Staciarini <staciarinineto@gmail.com>
2020-05-13 23:56:39 -0300

    Leitura da escolaridade

--- src/projeto_candidato/candidato.java
+++ src/projeto_candidato/candidato.java
@@ -1,5 +1,30 @@
 package projeto_candidato;
 
-public class candidato {
+// bibliotecas
+import java.util.Locale;
+import java.util.Scanner;
 
+public class candidato {
+	
+	public static void main(String[] args) {
+		    
+			Locale.setDefault(Locale.US); // idioma US
+			Scanner sc = new Scanner(System.in); // leitura
+			
+			int ESCOLARIDADE; // variavel
+			
+			// mostra as opções
+	        System.out.println("Qual sua escolaridade?");
+	        System.out.println("1) Ensino Fundamental");
+	        System.out.println("2) Ensino Médio");
+	        System.out.println("3) Ensino Superior");
+	        System.out.println("4) Pós-graduação");
+	        System.out.print("Digite uma opção: ");
+	        
+	        // ler a resposta
+	        ESCOLARIDADE = sc.nextInt();
+	        
+	        sc.close();
+	        
+	}        
 }
